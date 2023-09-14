e7fd9bd1f85907597f61624ba0b177e5bac4354c
taissa-rodrigues <taissavrp@gmail.com>
2023-09-14 13:08:10 -0300

    Initial Commit

---
+++ tela2-pokemon/tela2_pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  tela2_pokemonApp.swift
+//  tela2-pokemon
+//
+//  Created by user on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct tela2_pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
