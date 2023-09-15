a4bd8c0d64d4b5c1105373f767def83e90f64daf
taissa-rodrigues <141452801+taissa-rodrigues@users.noreply.github.com>
2023-09-15 16:39:01 -0300

    Final

--- tela2-pokemon/ContentView.swift
+++ tela2-pokemon/ContentView.swift
@@ -9,18 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+           Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("img-tela2.pokemon")
+                
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:600, height: 661)
+                  
+                    .edgesIgnoringSafeArea(.top)
+                Spacer()
+                HStack {
+                    ZStack {
+                        Text (" Ash Ketchum, com 10 anos, começa sua jornada como  treinador de Pokémon e recebe seu primeiro Pokémon, o Pikachu. Juntos, partem em busca do lendário Pokémon Ho-Oh, forjando uma amizade duradoura.")
+                            .font(.system(size: 19))
+                            .foregroundColor(.black)
+                            .frame(width: 380, height: 200)
+                            .background(Rectangle().fill(Color.white))
+                            .cornerRadius(30)
+                            .position(x:300, y:-50)
+                    }
+                }
+                    .overlay (
+
+                            Text ("▷  Assistir")
+                                
+                                .font(.system(size: 22))
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(
+                                    Rectangle()
+                                        .fill(Color.red)
+                                        .frame(width: 200, height: 65)
+                                        .cornerRadius(50)
+                                )
+                                .position(x: 300 , y: 55)
+                                )
+                
+                        
+                
+                        Text ("Pokémon O Filme: Escolho-te a Ti!")
+                    
+                            .font(.title2)
+                            .foregroundColor(.white)
+                      
+                            .padding()
+                            .position(x:290, y:-250)
+                                Text ("★ 5.0")
+                                .font(.title3)
+                                .foregroundColor(.white)
+                                .position(x: 152, y: -250)
+            }
         }
-        .padding()
-    }
-}
+     }
+               
+ }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
